33adc042219c30d496b9cc11881d97d819c38cf9
sedatguzelsemme <sedatguzelsemme@gmail.com>
2016-02-12 22:21:19 +0200

    Added core of application architecture

---
+++ GisCorp/GisCorp.Portal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GisCorp.Portal.Startup))]
+namespace GisCorp.Portal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
